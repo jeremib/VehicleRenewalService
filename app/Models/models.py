c3cf1fd6c015241914d81a84b913f76adbf71ed5
Aqab-pervaiz <meaqab@gmail.com>
2024-10-25 23:33:23 +0500

    Structured the code and remove unused data

---
+++ app/Models/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+# Request Body Model for FastAPI
+class QueryPriceRequest(BaseModel):
+    plateNumber: str
+    county: str
+    name: str
+    addressTwo: str
+    city: str
+    state: str
+    zip: str
+    homePhone0: str
+    homePhone1: str
+    homePhone2: str
+    email: str
+    confirmEmail: str
+
+class CompleteTransactionRequest(QueryPriceRequest):
+    account: str
+    exp_month: str
+    exp_year: str
+    cv: str
+
+# FastAPI Routes
